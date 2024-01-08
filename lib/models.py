448eade0ad5e8b3e7040b3ef10143c3391c657a7
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-08 11:39:09 +0300

    Table models

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table, Date
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -15,3 +15,41 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///hospital.db')
     Session = sessionmaker(bind = engine)
     session = Session()
+
+#Patient table
+class Patient(Base):
+    __tablename__ = 'patients'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    gender = Column(String, nullable=False)
+    contact_number = Column(String, nullable=False)
+
+    def __repr__ (self):
+        return f"{self.id}: {self.name}, DOB: {self.date_of_birth}, Gender: {self.gender}"
+
+# Appointment table
+class Appointment(Base):
+    __tablename__ = 'appointments'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    appointment_date = Column(Date, nullable=False)
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Appointment ID: {self.id}\n\tDate of Appointment: {self.appointment_date}"
+
+# medical staff table
+
+class MedicalStaff(Base):
+    __tablename__ = 'medical_staff'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    specialization = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"Name: {self.name}, Specialization: {self.specialization}"
+
+
